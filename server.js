4e5cdf00f25924f6ef2059c9fabdc096f3567202
SubhradipGbs <subhradipn@gbserp.com>
2025-12-02 11:59:57 +0530

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
